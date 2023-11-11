fc1c6e9bbcf7eac5f3c146060893bc581459b79d
Brandon Suarez <brandonsuarezduran@gmail.com>
2023-11-11 16:41:39 -0500

    Initial Commit

---
+++ FetchCodingChallenge/FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallenge
+//
+//  Created by Brandon Suarez on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
